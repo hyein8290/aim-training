da72694c2ead0198e60a87c0b1d7ac936c0ecafd
hyein8290 <hyein8290@gmail.com>
2022-05-13 17:29:06 +0900

    commit

--- GroupChatServer/src/kr/co/aim/server/EchoServer.java
+++ GroupChatServer/src/kr/co/aim/server/EchoServer.java
@@ -3,24 +3,29 @@ package kr.co.aim.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class EchoServer {
 
+	// TODO 주석으로 설명달기
+	private static HashMap<Long, HashMap<Long, User>> groups;
+	private static long groupSequence = 0L;	// 방번호
+	private static long userSequence = 0L; // 유저번호
+	private final int GROUP_NUM = 2; // 방  개수
+
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
 	private Scanner scanner;
-//	private List<Thread> list;
-	
-	private int port;
-	
+			
 	/**
 	 * EchoServer 생성자
 	 * serverSocket을 생성
 	 */
 	public EchoServer() {
+		setGroups(GROUP_NUM);
 		createServerSocket();
 	}
 
@@ -33,8 +38,10 @@ public class EchoServer {
 			System.out.println("[서버 시작] 클라이언트 대기중..");
 			
 			while(true) {
+				// 지역변수로?
 				clientSocket = serverSocket.accept();
-				EchoServerThread thread = new EchoServerThread(clientSocket);
+				HashMap<Long, User> group = joinGroup(clientSocket);
+				EchoServerThread thread = new EchoServerThread(clientSocket, group);
 				thread.start();
 			}
 			
@@ -48,12 +55,12 @@ public class EchoServer {
 	 */
 	private void createServerSocket() {
 		try {
-			System.out.println("================그룹서버dd================");
+			System.out.println("================그룹서버================");
 
 			scanner = new Scanner(System.in);
 			
 			System.out.print("Open할 Port번호를 입력해주세요: ");
-			port = scanner.nextInt();
+			int port = scanner.nextInt();
 
 			serverSocket = new ServerSocket(port);
 			
@@ -63,6 +70,34 @@ public class EchoServer {
 			System.out.println("0~65535 사이의 값을 입력해주세요.");
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * 클라이언트가 접속하면 그룹을 정해주도록 하자
+	 */
+	private HashMap<Long, User> joinGroup(Socket client) {
+		Long userId = ++userSequence;
+		Long groudId = userId % GROUP_NUM + 1;
+		
+		User user = new User();
+		user.setSocket(client);
+		
+		// 한 번만 꺼내자]
+		// 한번 꺼내서 확인한 다음에 
+		// get 했을 때 값이 없으면 null(에러 , 근데 null에 넣으려고 하면 에러
+		groups.get(groudId).put(userId, user);
+		
+		return groups.get(groudId);
+	}
+	
+	/*
+	 * 서버가 만들어지면 그룹들이 생성되도록 하자
+	 */
+	private void setGroups(int groupNums) {
+		groups = new HashMap<Long, HashMap<Long, User>>();
+		while(groupNums-- > 0) {
+			groups.put(++groupSequence, new HashMap<Long, User>());
 		}
 	}
 
--- GroupChatServer/src/kr/co/aim/server/EchoServerThread.java
+++ GroupChatServer/src/kr/co/aim/server/EchoServerThread.java
@@ -7,24 +7,36 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
+// TODO 인터페이스로 구현
 public class EchoServerThread extends Thread {
 	
 	private final int MAX_BUFFER_SIZE = 1024;
 	private final int HEADER_BYTE_COUNTS = 4;
+	byte[] bytes = new byte[MAX_BUFFER_SIZE];
+	
 	private Socket clientSocket;
 	private InputStream is;
 	private Scanner in;
 	private OutputStream out;
 	private String clientIP;
 	
+	// 동기화 문제 생각해보기
+	// ConcurrentMap<K,V> 속도, 동기화 문제 둘 다 해결
+	// HashTable
+	private HashMap<Long, User> myGroup;
+	
+	
 	/**
 	 * EchoServerThread 생성자
 	 * @param clientSocket	통신할 clientSocket
 	 */
-	public EchoServerThread(Socket clientSocket) {
+	public EchoServerThread(Socket clientSocket, HashMap<Long, User> group) {
 		this.clientSocket = clientSocket;
+		this.myGroup = group;
 	}
 	
 	/**
@@ -37,11 +49,13 @@ public class EchoServerThread extends Thread {
 			System.out.println("[클라이언트 접속] " + clientIP);
 
 			while(true) {
-				byte[] bytes = new byte[MAX_BUFFER_SIZE];
-				int messageByteCounts = receiveMessageByteCounts(bytes);
-				String message = receiveMessage(messageByteCounts);
+				int messageByteCounts = readMessageByteCounts();
+				//String message = receiveMessage(messageByteCounts);
+				bytes = receiveByteArray(messageByteCounts);
+				String message = in.nextLine();
 				System.out.println("[메시지 수신] " + clientIP + ": " + message);
-				sendMessage(bytes);
+				//sendMessage(bytes, messageByteCounts);
+				sendToGroup(messageByteCounts);
 			}
 		} catch (SocketException e) {
 			System.out.println("[클라이언트 퇴장] " + clientIP);
@@ -51,38 +65,57 @@ public class EchoServerThread extends Thread {
 			close();
 		}
 	}
+	
+	private byte[] receiveByteArray(int messageByteCounts) throws SocketException, IOException {
+		byte[] messageBytes = new byte[messageByteCounts];
+		is = clientSocket.getInputStream();
+		is.read(messageBytes);
+		in = new Scanner(new InputStreamReader(new ByteArrayInputStream(messageBytes, 0, messageByteCounts)));
+		return messageBytes;
+	}
 
 	private String receiveMessage(int messageByteCounts) throws SocketException, IOException {
-		byte[] bytes = new byte[messageByteCounts];
-		String output = "";		
+		byte[] messageBytes = new byte[messageByteCounts];
 
 		is = clientSocket.getInputStream();
-		is.read(bytes);
+		is.read(messageBytes);
 		
 		// 왜  header는 안 읽어올까? 이미 읽어서 그런가보다.
-		in = new Scanner(new InputStreamReader(new ByteArrayInputStream(bytes, 0, messageByteCounts)));
+		in = new Scanner(new InputStreamReader(new ByteArrayInputStream(messageBytes, 0, messageByteCounts)));
 		// in = new Scanner(new InputStreamReader(new ByteArrayInputStream(bytes, HEADER_BYTE_NUM, messageByteCounts)));
 		
-		output = in.nextLine();	
-
-		in.close();
-
+		String output = in.nextLine();	
+		//in.close();
 		return output;
 	}
 
-	/*
-	 * 바이트어레이 형식의 메시지를 보내는 메소드
+	/**
+	 * byte[] 형식의 메시지를 보내는 메소드
 	 */
-	private void sendMessage(byte[] bytes) throws SocketException, IOException {
+	private void sendMessage(byte[] bytes ,int messageByteCounts) throws SocketException, IOException {
 		out = clientSocket.getOutputStream();
-		out.write(bytes);
+		out.write(bytes, 0, messageByteCounts);
 		out.flush();
 	}
 	
-	/*
+	
+	// 여기서는 sync 해봤자.. 자료구조를 바꿔주자
+	private void sendToGroup(int messageByteCounts) throws SocketException, IOException {
+		Iterator iterator = myGroup.keySet().iterator();
+		
+		while(iterator.hasNext()) {
+			out = myGroup.get(iterator.next()).getSocket().getOutputStream();
+			
+			// TODO 아직 메시지만, 클라이언트에서 헤더 구분 하는 거 안함
+			out.write(bytes, 0, messageByteCounts);
+			out.flush();
+		}
+	}
+	
+	/**
 	 * byte[] 형식의 메시지를 받아서 String으로 반환하는 메소드
 	 */
-	private int receiveMessageByteCounts(byte[] bytes) throws SocketException, IOException {
+	private int readMessageByteCounts() throws SocketException, IOException {
 		byte[] headerBytes = new byte[HEADER_BYTE_COUNTS];
 		is = clientSocket.getInputStream();
 		is.read(headerBytes);
