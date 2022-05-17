5a94dfa908de117386af7cb94063f171c53e2362
hyein8290 <hyein8290@gmail.com>
2022-05-17 14:41:15 +0900

    콤보박스 수정 안 되게 변경

--- GroupChatClient/ChatClient/ChatForm.Designer.cs
+++ GroupChatClient/ChatClient/ChatForm.Designer.cs
@@ -36,33 +36,38 @@
             // 
             // cboMessageNum
             // 
+            this.cboMessageNum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboMessageNum.FormattingEnabled = true;
+            this.cboMessageNum.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.cboMessageNum.Items.AddRange(new object[] {
             "All",
             "5",
             "10",
             "20",
             "50"});
-            this.cboMessageNum.Location = new System.Drawing.Point(326, 5);
+            this.cboMessageNum.Location = new System.Drawing.Point(373, 6);
+            this.cboMessageNum.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cboMessageNum.Name = "cboMessageNum";
-            this.cboMessageNum.Size = new System.Drawing.Size(121, 20);
+            this.cboMessageNum.Size = new System.Drawing.Size(138, 23);
             this.cboMessageNum.TabIndex = 1;
-            this.cboMessageNum.Text = "All";
+            this.cboMessageNum.SelectedIndex = 0;
             this.cboMessageNum.SelectedIndexChanged += new System.EventHandler(this.cboMessageNum_SelectedIndexChanged);
             // 
             // messageTextBox
             // 
-            this.messageTextBox.Location = new System.Drawing.Point(6, 384);
+            this.messageTextBox.Location = new System.Drawing.Point(7, 480);
+            this.messageTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.messageTextBox.Multiline = true;
             this.messageTextBox.Name = "messageTextBox";
-            this.messageTextBox.Size = new System.Drawing.Size(340, 103);
+            this.messageTextBox.Size = new System.Drawing.Size(388, 128);
             this.messageTextBox.TabIndex = 3;
             // 
             // sendBtn
             // 
-            this.sendBtn.Location = new System.Drawing.Point(352, 384);
+            this.sendBtn.Location = new System.Drawing.Point(402, 480);
+            this.sendBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.sendBtn.Name = "sendBtn";
-            this.sendBtn.Size = new System.Drawing.Size(95, 103);
+            this.sendBtn.Size = new System.Drawing.Size(109, 129);
             this.sendBtn.TabIndex = 4;
             this.sendBtn.Text = "전송";
             this.sendBtn.UseVisualStyleBackColor = true;
@@ -71,23 +76,25 @@
             // txtMessage
             // 
             this.txtMessage.Enabled = false;
-            this.txtMessage.Location = new System.Drawing.Point(6, 31);
+            this.txtMessage.Location = new System.Drawing.Point(7, 39);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
             this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtMessage.Size = new System.Drawing.Size(441, 347);
+            this.txtMessage.Size = new System.Drawing.Size(503, 433);
             this.txtMessage.TabIndex = 5;
             // 
             // ChatForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.sendBtn);
             this.Controls.Add(this.messageTextBox);
             this.Controls.Add(this.cboMessageNum);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ChatForm";
-            this.Size = new System.Drawing.Size(450, 500);
+            this.Size = new System.Drawing.Size(514, 625);
             this.Load += new System.EventHandler(this.ChatForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
