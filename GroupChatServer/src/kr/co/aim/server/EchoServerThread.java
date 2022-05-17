799828e6dbff227e51b888b9794971f7d6fa0a53
hyein8290 <hyein8290@gmail.com>
2022-05-17 15:08:51 +0900

    out close 삭제

--- GroupChatServer/src/kr/co/aim/server/EchoServerThread.java
+++ GroupChatServer/src/kr/co/aim/server/EchoServerThread.java
@@ -107,7 +107,6 @@ public class EchoServerThread implements Runnable {
 	private void close() {
 		try {
 			is.close();
-			out.close();
 			clientSocket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
