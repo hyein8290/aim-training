bf932ad7bc22e8f5302784cb1ed1aed2129bb9e1
hyein8290 <hyein8290@gmail.com>
2022-05-27 03:07:11 +0900

    다시 따라서 잘 해보자

---
+++ shadow-server/src/main/java/kr/co/aim/shadowserver/ShadowServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.aim.shadowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowServerApplication.class, args);
+	}
+
+}
