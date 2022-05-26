e6b072a201c2e1d0515f486612cc37d24c5a13e5
hyein8290 <hyein8290@gmail.com>
2022-05-27 02:07:45 +0900

    다시 침착하게 해보자

---
+++ simple-server/src/test/java/kr/co/aim/simpleserver/SimpleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.aim.simpleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
