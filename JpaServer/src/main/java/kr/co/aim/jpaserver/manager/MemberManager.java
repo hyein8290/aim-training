411ca5cb0a688e25e6e784948547aed85a0e6917
hyein8290 <hyein8290@gmail.com>
2022-06-01 14:25:22 +0900

    MemberManager 구현

--- JpaServer/src/main/java/kr/co/aim/jpaserver/manager/MemberManager.java
+++ JpaServer/src/main/java/kr/co/aim/jpaserver/manager/MemberManager.java
@@ -1,57 +1,41 @@
 package kr.co.aim.jpaserver.manager;
 
-import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.google.common.collect.Maps;
-
 import kr.co.aim.jpaserver.data.Member;
-//import kr.co.aim.jpaserver.utils.IDGenerator;
+import kr.co.aim.jpaserver.repository.MemberRepository;
 
 @Component
 public class MemberManager {
 
-//	// 왜 자바에서 제공하는 해시맵 안 쓰셨을까? -> 직관적, 버그 발생률 낮음, 편함
-//	private final Map<Integer, Member> memberMap = Maps.newHashMap();
-//	private final ReentrantLock lock = new ReentrantLock();
-//
-//	@Autowired
-//	private RoomManager roomManager;
-//
-//	public Member create() {
-//
-//		int roomId = roomManager.getRoomId(clientId);
-//
-//		Member member = this.memberMap.get(clientId);
-//		if (member == null) {
-//			member = new Member();
-//			member.setId(clientId);
-//			member.setRoomId(roomId);
-//			this.memberMap.put(member.getId(), member);
-//			System.out.printf("유저의 아이디(방아이디): %s(%s)\n", member.getId(), member.getRoomId());
-//		}
-//
-//		return member;
-//	}
-//
-//	public Member getMember(int id) {
-//		this.lock.lock();
-//		try {
-//			return this.memberMap.get(id);
-//		} finally {
-//			this.lock.unlock();
-//		}
-//	}
-//
-//	public void remove(int id) {
-//		this.lock.lock();
-//		try {
-//			this.memberMap.remove(id);
-//		} finally {
-//			this.lock.unlock();
-//		}
-//	}
+	private final MemberRepository memberRepository;
+	
+	public MemberManager(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	/*
+	 * member를 만드는 메소드
+	 * 1. id는 db에서 자동 생성
+	 * 2. 이름, 방 넣어줘야겠네
+	 * 3. 방 넣어주려면 inroomManager를 사용해야 되나
+	 * 4. 우선 방 넣어주고, 나중에 클라이언트가 직접 방 만들고 들어갈 수 있도록 하자
+	 * 
+	 * 5. 다 아니야 그냥 member 테이블에만 넣자
+	 */
+	public Member create() {
+		Member member = new Member();
+		return memberRepository.save(member);
+	}
+
+	public Member getMember(int memberId) {
+		return memberRepository.getById(memberId);
+	}
+
+	// member를 참조하는 inroom, message 먼저 삭제
+	// TODO 구현
+	public void remove(int memberId) {
+		memberRepository.deleteById(memberId);
+	}
 }
